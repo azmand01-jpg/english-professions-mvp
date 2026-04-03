a2070d4f4bfc8b6595d1eb653537dd032353a81e
azmand01-jpg <azmand01@hotmail.com>
2026-04-03 00:59:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,69 +1,78 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
+app.use(express.static(__dirname));
 
-const db = new sqlite3.Database('./database.db');
-
-// INIT DB (run only once)
-const fs = require('fs');
-if (!fs.existsSync('./database.db')) {
-    const initSQL = fs.readFileSync('./init.sql', 'utf8');
-    db.exec(initSQL);
-    console.log("Database created");
-}
-
-// ROUTES
-
-// get professions
-app.get('/professions', (req, res) => {
-    db.all("SELECT * FROM professions", [], (err, rows) => {
-        res.json(rows);
-    });
+const db = new sqlite3.Database('./database.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log('Database connected');
+  }
 });
 
-// get scenarios by profession
+// Home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Get scenarios by profession
 app.get('/scenarios', (req, res) => {
-    const code = req.query.profession;
+  const code = req.query.profession;
 
-    db.all(
-        "SELECT * FROM scenarios WHERE profession_code = ?",
-        [code],
-        (err, rows) => {
-            res.json(rows);
-        }
-    );
+  db.all(
+    "SELECT * FROM scenarios WHERE profession_code = ?",
+    [code],
+    (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json(rows);
+    }
+  );
 });
 
-// get single scenario (FULL DESCRIPTION)
+// Get single scenario
 app.get('/scenario', (req, res) => {
-    const id = req.query.id;
+  const id = req.query.id;
 
-    db.get(
-        "SELECT * FROM scenarios WHERE id = ?",
-        [id],
-        (err, row) => {
-            res.json(row);
-        }
-    );
+  db.get(
+    "SELECT * FROM scenarios WHERE id = ?",
+    [id],
+    (err, row) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json(row);
+    }
+  );
 });
 
-// get keywords
+// Get keywords for scenario
 app.get('/keywords', (req, res) => {
-    const scenarioId = req.query.scenario;
+  const scenarioId = req.query.scenario;
 
-    db.all(
-        "SELECT * FROM keywords WHERE scenario_id = ?",
-        [scenarioId],
-        (err, rows) => {
-            res.json(rows);
-        }
-    );
+  db.all(
+    "SELECT word, pronunciation FROM keywords WHERE scenario_id = ?",
+    [scenarioId],
+    (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json(rows);
+    }
+  );
 });
 
-// start server
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
